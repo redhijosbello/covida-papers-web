b974f8450e5003a15c951ef1a289e0dd8d02e988
martinKindall <mart256@gmail.com>
2020-04-10 19:57:06 -0400

    Added index.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import FormResultsContainer from './components/FormResultsContainer';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap-css-only/css/bootstrap.min.css';
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
