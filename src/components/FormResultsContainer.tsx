8b0cd24a1cb1b952108ee68fd9a9aba864d98953
martinKindall <mart256@gmail.com>
2020-04-10 22:39:32 -0400

    Changed form content.

--- src/components/FormResultsContainer.tsx
+++ src/components/FormResultsContainer.tsx
@@ -7,6 +7,13 @@ import Button from 'react-bootstrap/Button';
 
 
 function FormResultsContainer() {
+
+  const handleSubmit = (event: any) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
   return (
     <Container>
       <Row>
@@ -14,24 +21,18 @@ function FormResultsContainer() {
           <p className="text-center h2 myColorPrimary font-weight-bold">COVIDA</p>
         </Col>
       </Row>
-      <Row>
+      <Row className="mt-3">
         <Col sm={12} md={6}>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="titleKeyword">
+              <Form.Label>Palabra en título</Form.Label>
+              <Form.Control type="text" placeholder="Ej: mask" />
             </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group controlId="contentKeyword">
+              <Form.Label>Palabra en contenido</Form.Label>
+              <Form.Control type="text" placeholder="Ej: covid-19" />
             </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button type="submit">
               Submit
             </Button>
           </Form>
