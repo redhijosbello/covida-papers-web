3a94cf390aed7c88a54a10f92764bac2b35b1dde
martinKindall <mart256@gmail.com>
2020-04-10 20:14:58 -0400

    Added form and changed background color.

--- src/components/FormResultsContainer.tsx
+++ src/components/FormResultsContainer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 function FormResultsContainer() {
@@ -9,7 +11,30 @@ function FormResultsContainer() {
     <Container>
       <Row>
         <Col xs={12}>
-          <p className="text-center h2 text-white font-weight-bold">COVIDA</p>
+          <p className="text-center h2 myColorPrimary font-weight-bold">COVIDA</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={6}>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Col>
       </Row>
     </Container>
