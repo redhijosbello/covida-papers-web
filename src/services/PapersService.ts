3d75d24514a33f16c9c2a48c74d888c20ffe020c
martinKindall <mart256@gmail.com>
2020-04-13 13:05:02 -0400

    PapersService: fetching the papers server endpoint.

--- src/services/PapersService.ts
+++ src/services/PapersService.ts
@@ -1,10 +1,36 @@
 import {PaperSource} from "../enums/PaperSource";
-import {dummyPapers} from "../dummy/PapersDummy";
 import {PaperData} from "../dataClasses/PaperData";
+import axios from 'axios';
 
 export default class PapersService {
 
+  private sourceEndpointsAdapter = (source: PaperSource): string => {
+    switch (source) {
+      case PaperSource.ARXIV:
+        return "/arxiv";
+      case PaperSource.GSCHOLAR:
+        return '/googlescholar';
+      case PaperSource.LANCET:
+        return '/lancet';
+      case PaperSource.MBIO:
+        return '/mbio';
+    }
+  };
+
   public fetch(titleParam: string, contentParam: string, source: PaperSource): Promise<PaperData[]> {
-    return Promise.resolve(dummyPapers);
+    return axios.get(process.env.REACT_APP_API_ENDPOINT + this.sourceEndpointsAdapter(source) + '/papersOfInterest', {
+      params: {
+        'word_in_title': titleParam,
+        'word_in_paper': contentParam,
+      }
+    })
+      .then(results => {
+      return results.data;
+    })
+      .catch((error: any) => {
+        console.log(error);
+        alert("Hubo un error, favor intentar más tarde.");
+        return [];
+    });
   }
 }
