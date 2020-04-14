f8808542215a382dff84c0d4f37bbcaeb95e0c45
martinKindall <mart256@gmail.com>
2020-04-13 21:55:22 -0400

    Added build to project

---
+++ build/precache-manifest.47ae6a33ec88976d3c255ec95c748fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2067e1a68defc4d4b3b704a7c0107df4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1144e3050562391f278",
+    "url": "/static/css/main.99ac76e0.chunk.css"
+  },
+  {
+    "revision": "d9a305f91339191fd1a6",
+    "url": "/static/js/2.ff093063.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ff093063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1144e3050562391f278",
+    "url": "/static/js/main.0347fee1.chunk.js"
+  },
+  {
+    "revision": "4247e22174cb72a7b715",
+    "url": "/static/js/runtime-main.2a63b282.js"
+  }
+]);
